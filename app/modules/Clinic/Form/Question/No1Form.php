245534f5378819a126d0ea350f8831d01e5ded88
myethical <myethical>
2016-09-05 18:07:52 +0700

    Revert "add update"
    This reverts commit ba3d0c89cfac83beb229448e0734ae96f871b7b2.

---
+++ app/modules/Clinic/Form/Question/No1Form.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Clinic\Form\Question;
+
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\Select;
+use Clinic\Model\Office;
+use Phalcon\Validation\Validator\PresenceOf;
+
+class No1Form extends \Phalcon\Forms\Form
+{
+
+    public function initialize()
+    {
+        $no1_1_2 = new Text('no1_1_2', array(
+            'required' => true,
+            'placeholder' => 'กรอกคำถามพื้นที่',
+            'class' =>"form-control col-md-3 col-xs-6"
+        ));
+        $this->add($no1_1_2);
+
+        $no1_1_3_1 = new Select('no1_1_3_1', 
+            Office::find(),
+            array(
+                'using' => array(
+                    'id',
+                    'name'
+                ),
+                'class'=>"select2_multiple form-control",
+                'placeholder'=> "Select a state",
+                'multiple'=>"multiple"
+                )
+            );
+        $this->add($no1_1_3_1);
+
+
+        $no1_1_3_2 = new Select('no1_1_3_2', 
+            Office::find(),
+            array(
+                'using' => array(
+                    'id',
+                    'name'
+                ),
+                'class'=>"select2_multiple form-control",
+                'placeholder'=> "Select a state",
+                'multiple'=>"multiple"
+                )
+            );
+        $this->add($no1_1_3_2);
+
+        $no1_1_3_3 = new Select('no1_1_3_3', 
+            Office::find(),
+            array(
+                'using' => array(
+                    'id',
+                    'name'
+                ),
+                'class'=>"select2_multiple form-control",
+                'placeholder'=> "Select a state",
+                'multiple'=>"multiple"
+                )
+            );
+        $this->add($no1_1_3_3);
+
+        $no1_1_3_4 = new Select('no1_1_3_4', 
+            Office::find(),
+            array(
+                'using' => array(
+                    'id',
+                    'name'
+                ),
+                'class'=>"select2_multiple form-control",
+                'placeholder'=> "Select a state",
+                'multiple'=>"multiple"
+                )
+            );
+        $this->add($no1_1_3_4);
+
+
+    }
+
+}
