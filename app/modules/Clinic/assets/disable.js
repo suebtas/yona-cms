6de15ae7182feb9bb8fd538f3836d99e73c7d52a
suebtas <suebtas@gmail.com>
2017-12-10 23:40:25 +0700

    fix approve enable
    สร้างยกเลิกอนุมัติได้

--- app/modules/Clinic/assets/disable.js
+++ app/modules/Clinic/assets/disable.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() { 
     $(".editable").editable('option', 'disabled', true);
-    
+    /*
     $("input[name='approve']").each(function(i) {
         $(this).attr('disabled', 'disabled');
     });
-    
+    */
     $("select").each(function(i) {
         $(this).attr('disabled', 'disabled');
     });
